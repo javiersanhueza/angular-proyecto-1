e25d475aa6213e57a3a3f9f462eeff8e286779c8
Javier Sanhueza Mejías <javier.sanhueza@nextcode.cl>
2023-08-28 15:10:56 -0400

    ngAfterContentInit

--- src/app/components/my-component2/my-component2.component.ts
+++ src/app/components/my-component2/my-component2.component.ts
@@ -1,8 +1,33 @@
-import { Component } from "@angular/core";
+import {
+  AfterContentInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ContentChild
+} from "@angular/core";
 
 @Component({
   selector: "app-my-component2",
   templateUrl: "./my-component2.component.html",
   styleUrls: ["./my-component2.component.scss"]
 })
-export class MyComponent2Component {}
+export class MyComponent2Component implements OnInit, OnChanges, DoCheck, AfterContentInit {
+  @ContentChild("childComponent2") contentChild: HTMLElement | undefined;
+  ngAfterContentInit(): void {
+    console.log("**ngAfterContentInit COMPONENTE 2**");
+    console.log(this.contentChild);
+  }
+
+  ngDoCheck(): void {
+    console.log("**ngDoCheck COMPONENTE 2**");
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {}
+
+  ngOnInit(): void {
+    console.log(this.contentChild);
+    console.log("**ngOnInit COMPONENTE 2**");
+  }
+}
